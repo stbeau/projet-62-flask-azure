8adb4cba63b5b716de1abbe5eb50ef63ad553119
sbeau <1995282@bdeb.qc.ca>
2020-10-28 21:26:24 -0400

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,41 +1,44 @@
 from app import app
 from flask import render_template
 import pickle, numpy as np
+import pandas as pd
 from joblib import dump, load
 #from sklearn.linear_model import LinearRegression
 
-@app.route('/')
-@app.route('/index')
-def index():
-    modeles = {'titre': 'KNN'}
-    parametres = [
-        {"par1":"10", "par2":"uniform"},
-        {"par1":"20", "par2":"lineaire"}
-    ]
-    return render_template('index.html', titre_page='Accueil', mod=modeles, page_no=1, param=parametres)
+# @app.route('/')
+# @app.route('/index')
+# def index():
+    # modeles = {'titre': 'France'}
+    # parametres = [
+        # {"par1":"10", "par2":"uniform"},
+        # {"par1":"20", "par2":"lineaire"}
+    # ]
+    # return render_template('index.html', titre_page='Accueil', mod=modeles, page_no=1, param=parametres)
 
 from app.forms import ModelePredictionForm
 
 from flask import  flash, redirect
 
-@app.route('/form_input', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def form_input():
     form = ModelePredictionForm()
     if form.validate_on_submit():
-        #formuler le data
-        sample_data = [form.taille.data, form.poids.data, form.pointure.data]
-        clean_data = [float(i) for i in sample_data]
-        data = []
-        data.extend(clean_data[0:3])
-        # Reshape the Data
-        ex1 = np.array(data).reshape(1, -1)
-        #lire le modele
-        #rl_model = pickle.load(open('modele_LR.pkl', 'rb'))
-        rl_model = load("knn.joblib")
-        resultat_prediction = rl_model.predict(ex1)
-        flash('Taille: {}, Poids:{}, Pointure:{} donne Y={}'.format(
-            form.taille.data, form.poids.data, form.pointure.data, resultat_prediction))
-        return redirect('/index')
+
+        colnames = ["latitude","longitude","nombre_pieces_principales","surface_reelle_bati","surface_terrain","type_local"]
+        sample_data = [form.latitude.data, form.longitude.data,
+                        form.nombre_pieces_principales.data, form.surface_reelle_bati.data,
+                        form.surface_terrain.data,form.type_local.data]
+        X_df = pd.DataFrame([sample_data],columns=colnames)
+        
+        
+        pred_model = load("model.joblib")
+        resultat_prediction = pred_model.predict(X_df)
+        resultat_prediction = np.expm1(resultat_prediction[0])
+        prediction_text="Valeur estimée : {:.0f} $".format(resultat_prediction)
+
+        flash(prediction_text)
+
+        #return redirect('/')
     return render_template('form_input.html', title='Modele de Prediction', form=form)
 
 
